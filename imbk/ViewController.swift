6970264ca9ed616069fbd76ca646d66bc7498108
Andrew Ferrier <andrewferrier@example.com>
2016-05-10 17:37:48 +0100

    Very basic functionality to iterate through photos.

--- imbk/ViewController.swift
+++ imbk/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Photos
 
 class ViewController: UIViewController {
 
@@ -20,6 +21,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func backupPhotos(sender: UIButton) {
+        // TODO: Exclude videos
+        
+        var assets = PHAsset.fetchAssetsWithMediaType(PHAssetMediaType.Image, options: nil)
+        
+        assets.enumerateObjectsUsingBlock { (obj, idx, bool) -> Void in
+            PHImageManager.defaultManager().requestImageDataForAsset(obj as! PHAsset, options: nil)
+            {
+                imageData,dataUTI,orientation,info in self.uploadPhoto(imageData!)
+            }
+        }
+    }
+    
+    func uploadPhoto(x: AnyObject) {
+        print(x)
+    }
 }
 
