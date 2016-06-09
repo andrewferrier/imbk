e8b3f5dfb3e833c6290f2ba6a4f4f129df4262a3
Andrew Ferrier <andrewferrier@example.com>
2016-06-09 19:45:54 +0100

    Add local "reminder" notification - closes #11

--- imbk/AppDelegate.swift
+++ imbk/AppDelegate.swift
@@ -10,12 +10,15 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
-
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+
+        NSLog("About to ask for permission for notifications.")
+
+        application.registerUserNotificationSettings(UIUserNotificationSettings(forTypes: [UIUserNotificationType.Sound, UIUserNotificationType.Alert,
+            UIUserNotificationType.Badge], categories: nil))
         return true
     }
 
@@ -27,10 +30,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+
+        let notification = UILocalNotification()
+        notification.timeZone = NSTimeZone.defaultTimeZone()
+        let dateTime = NSDate(timeIntervalSinceNow: 60 * 60 * 24) // 1 day
+        notification.fireDate = dateTime
+        notification.alertBody = "Time to backup your photos with imbk!"
+        notification.applicationIconBadgeNumber = 1
+        UIApplication.sharedApplication().scheduleLocalNotification(notification)
+        NSLog("Local notification scheduled.")
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+
+        UIApplication.sharedApplication().cancelAllLocalNotifications()
+        UIApplication.sharedApplication().applicationIconBadgeNumber = 0
+        NSLog("All local notifications cancelled.")
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
@@ -40,7 +56,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
 
