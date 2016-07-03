6da0b3ba0848b1e8d68a16562ea3f19d00fbb130
Andrew Ferrier <andrewferrier@example.com>
2016-07-03 13:57:19 +0800

    Remove automated fetching of provisioning profile to avoid 2-step verification.

--- fastlane/local_fastlane_testing.sh
+++ fastlane/local_fastlane_testing.sh
@@ -4,8 +4,6 @@ export KEY_PASSWORD=XXX
 export ENCRYPTION_PASSWORD=XXX
 export HOCKEY_API_TOKEN=XXX
 export HOCKEY_APP_ID=XXX
-export FASTLANE_PASSWORD=XXX # Apple User ID
-export FASTLANE_USERID=XXX # Apple Password
-export TEAM_ID=XXX
 
 openssl aes-256-cbc -k $ENCRYPTION_PASSWORD -in fastlane/Certificates/distribution.p12.enc -d -a -out fastlane/Certificates/distribution.p12
+openssl aes-256-cbc -k $ENCRYPTION_PASSWORD -in fastlane/Certificates/Development_com.andrewferrier.imbk.mobileprovision.enc -d -a -out fastlane/Certificates/Development_com.andrewferrier.imbk.mobileprovision
