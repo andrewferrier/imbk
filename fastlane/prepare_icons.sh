d1c29909392df303fb3d7a5994a8f63d09e8afee
Andrew Ferrier <andrewferrier@example.com>
2016-07-03 21:07:56 +0800

    Clean up icon.

--- fastlane/prepare_icons.sh
+++ fastlane/prepare_icons.sh
@@ -5,19 +5,20 @@
 ABSOLUTE_PATH=$(cd $(dirname "$0"); pwd)
 
 LOCATION="$ABSOLUTE_PATH/../imbk/Assets.xcassets/AppIcon.appiconset/"
+MODEL=$ABSOLUTE_PATH/../model/
 
-convert -resize '29x29'   -unsharp 1x4 $ABSOLUTE_PATH/../model/imbk-icon.svg "$LOCATION/Icon-Small.png"
-convert -resize '58x58'   -unsharp 1x4 $ABSOLUTE_PATH/../model/imbk-icon.svg "$LOCATION/Icon-Small@2x.png"
-convert -resize '87x87'   -unsharp 1x4 $ABSOLUTE_PATH/../model/imbk-icon.svg "$LOCATION/Icon-Small@3x.png"
+convert -font $MODEL/OpenSans.ttf -resize '29x29'   -unsharp 1x4 $MODEL/imbk-icon.svg "$LOCATION/Icon-Small.png"
+convert -font $MODEL/OpenSans.ttf -resize '58x58'   -unsharp 1x4 $MODEL/imbk-icon.svg "$LOCATION/Icon-Small@2x.png"
+convert -font $MODEL/OpenSans.ttf -resize '87x87'   -unsharp 1x4 $MODEL/imbk-icon.svg "$LOCATION/Icon-Small@3x.png"
 
-convert -resize '40x40'   -unsharp 1x4 $ABSOLUTE_PATH/../model/imbk-icon.svg "$LOCATION/Icon-40.png"
-convert -resize '80x80'   -unsharp 1x4 $ABSOLUTE_PATH/../model/imbk-icon.svg "$LOCATION/Icon-40@2x.png"
-convert -resize '120x120' -unsharp 1x4 $ABSOLUTE_PATH/../model/imbk-icon.svg "$LOCATION/Icon-40@3x.png"
+convert -font $MODEL/OpenSans.ttf -resize '40x40'   -unsharp 1x4 $MODEL/imbk-icon.svg "$LOCATION/Icon-40.png"
+convert -font $MODEL/OpenSans.ttf -resize '80x80'   -unsharp 1x4 $MODEL/imbk-icon.svg "$LOCATION/Icon-40@2x.png"
+convert -font $MODEL/OpenSans.ttf -resize '120x120' -unsharp 1x4 $MODEL/imbk-icon.svg "$LOCATION/Icon-40@3x.png"
 
-convert -resize '120x120' -unsharp 1x4 $ABSOLUTE_PATH/../model/imbk-icon.svg "$LOCATION/Icon-60@2x.png"
-convert -resize '180x180' -unsharp 1x4 $ABSOLUTE_PATH/../model/imbk-icon.svg "$LOCATION/Icon-60@3x.png"
+convert -font $MODEL/OpenSans.ttf -resize '120x120' -unsharp 1x4 $MODEL/imbk-icon.svg "$LOCATION/Icon-60@2x.png"
+convert -font $MODEL/OpenSans.ttf -resize '180x180' -unsharp 1x4 $MODEL/imbk-icon.svg "$LOCATION/Icon-60@3x.png"
 
-convert -resize '76x76'   -unsharp 1x4 $ABSOLUTE_PATH/../model/imbk-icon.svg "$LOCATION/Icon-76.png"
-convert -resize '152x152' -unsharp 1x4 $ABSOLUTE_PATH/../model/imbk-icon.svg "$LOCATION/Icon-76@2x.png"
+convert -font $MODEL/OpenSans.ttf -resize '76x76'   -unsharp 1x4 $MODEL/imbk-icon.svg "$LOCATION/Icon-76.png"
+convert -font $MODEL/OpenSans.ttf -resize '152x152' -unsharp 1x4 $MODEL/imbk-icon.svg "$LOCATION/Icon-76@2x.png"
 
-convert -resize '167x167' -unsharp 1x4 $ABSOLUTE_PATH/../model/imbk-icon.svg "$LOCATION/Icon-83.5@2x.png"
+convert -font $MODEL/OpenSans.ttf -resize '167x167' -unsharp 1x4 $MODEL/imbk-icon.svg "$LOCATION/Icon-83.5@2x.png"
