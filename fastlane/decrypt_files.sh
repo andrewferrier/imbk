cbe10b988aaf2bf04e36c1a76fa0a565bb9785e7
Andrew Ferrier <andrewferrier@example.com>
2016-07-03 14:55:01 +0800

    Ensure there is a provisioning profile which matches signing certs.

--- fastlane/decrypt_files.sh
+++ fastlane/decrypt_files.sh
@@ -4,4 +4,4 @@ ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 openssl aes-256-cbc -k $ENCRYPTION_PASSWORD -in $ABSOLUTE_PATH/Certificates/distribution.p12.enc -d -a -out $ABSOLUTE_PATH/Certificates/distribution.p12
 openssl aes-256-cbc -k $ENCRYPTION_PASSWORD -in $ABSOLUTE_PATH/Certificates/distribution.cer.enc -d -a -out $ABSOLUTE_PATH/Certificates/distribution.cer
-openssl aes-256-cbc -k $ENCRYPTION_PASSWORD -in $ABSOLUTE_PATH/Certificates/Development_com.andrewferrier.imbk.mobileprovision.enc -d -a -out $ABSOLUTE_PATH/Certificates/Development_com.andrewferrier.imbk.mobileprovision
+openssl aes-256-cbc -k $ENCRYPTION_PASSWORD -in $ABSOLUTE_PATH/Certificates/imbkDistribution.mobileprovision.enc -d -a -out $ABSOLUTE_PATH/Certificates/imbkDistribution.mobileprovision
