15a64e42ad0d39745d8ce8da65000d416a8e28aa
Andrew Ferrier <andrewferrier@example.com>
2016-08-05 00:04:57 +0100

    Add template for BMS_ACCESS_KEY.

--- fastlane/local_fastlane_testing.sh
+++ fastlane/local_fastlane_testing.sh
@@ -6,6 +6,6 @@ export KEY_PASSWORD=XXX
 export ENCRYPTION_PASSWORD=XXX
 export HOCKEY_API_TOKEN=XXX
 export HOCKEY_APP_ID=XXX
-
+export BMS_ACCESS_KEY=XXX
 
 (cd $ABSOLUTE_PATH && ./decrypt_files.sh)
